71e58d977d088721bfd2af17de6d3ddbd1b68789
Brian Ford <btford@umich.edu>
2013-07-03 10:51:24 -0700

    check only files, and eliminate double reporting

--- tasks/merge-conflict.js
+++ tasks/merge-conflict.js
@@ -6,12 +6,17 @@ var disallowed = [
   '>>>>>>>'
 ];
 
+var fs = require('fs');
+
 module.exports = function (grunt) {
 
   grunt.registerMultiTask('merge-conflict', 'Check for merge conflict markers', function () {
     var failed = false;
     this.files.forEach(function (glob) {
       glob.src
+        .filter(function (file) {
+          return fs.statSync(file).isFile();
+        })
         .map(function (file) {
           return {
             name: file,
@@ -19,10 +24,10 @@ module.exports = function (grunt) {
           };
         })
         .forEach(function (file) {
-          disallowed.forEach(function (thing) {
-            if (file.contents.indexOf(thing) !== -1) {
-              var line = file.contents.substr(0, file.contents.indexOf(thing)).split('\n').length;
-              grunt.log.errorlns(file.name + ' has merge conflict marker at line ' + line);
+          var lines = file.contents.split('\n');
+          lines.forEach(function (line, num) {
+            if (disallowed.indexOf(line.substr(0, 7)) !== -1) {
+              grunt.log.errorlns(file.name + ' has merge conflict marker at line ' + (num + 1));
               failed = true;
             }
           });
