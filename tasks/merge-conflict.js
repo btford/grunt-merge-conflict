3dd662e90a751eb989d792677296d796cd065c5f
Brian Ford <btford@umich.edu>
2013-07-08 17:11:26 -0700

    refactor task

--- tasks/merge-conflict.js
+++ tasks/merge-conflict.js
@@ -1,24 +1,21 @@
 'use strict';
-var fs = require('fs');
-var disallowed = [
-  '<<<<<<<',
-  '=======',
-  '>>>>>>>'
-];
+
+var checkFile = require('./lib/check-file');
 
 module.exports = function (grunt) {
   grunt.registerMultiTask('merge-conflict', 'Check for merge conflict markers', function () {
     var done = this.async();
     grunt.util.async.forEach(this.filesSrc, function (file, next) {
-      file.src.forEach(function (file) {
-        var lines = grunt.file.read(file).contents.split('\n');
-        lines.forEach(function (line, num) {
-          if (disallowed.indexOf(line.substr(0, 7)) !== -1) {
-            grunt.log.errorlns(file + ' has merge conflict marker at line ' + (num + 1));
-            next(true);
-          }
-        });
-      });
+      if (grunt.file.isFile(file)) {
+        var fileContents = grunt.file.read(file);
+        var errs;
+        if (errs = checkFile(fileContents)) {
+          errs.forEach(function (err) {
+            grunt.log.errorlns(file + ' has merge conflict marker at line ' + err);
+          });
+          next(true);
+        }
+      }
       next();
     }, function (failed) {
       if (failed) {
